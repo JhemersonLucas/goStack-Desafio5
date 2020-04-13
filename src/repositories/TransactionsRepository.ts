e5136f21d4ce81c3223236e97675ba932da1aa85
Jhemerson Lucas <jhemersonlucas@hotmail.com>
2020-04-13 14:38:46 -0300

    Firt Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string,
+  value: number, 
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    let balance:Balance;
+
+    const income = this.transactions.reduce((accumulator: number, current)=> {
+      if(current.type == 'income') return accumulator + current.value;
+      else return accumulator;
+    }, 0);
+
+    const outcome = this.transactions.reduce((accumulator: number, current)=> {
+      if(current.type == 'outcome') return accumulator + current.value;
+      else return accumulator;
+    }, 0);
+
+    balance = {
+      income, outcome, total: income - outcome
+    }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type} : Request ): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
